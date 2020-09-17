78420baedb06a4834dcf65607d5a9dd245d0ad88
Priscillah Nalubega <49272077+priscillahnalubega@users.noreply.github.com>
2020-09-17 22:02:19 +0300

    Delete answers.js

--- API/routes/answers.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.post('/', (req, res, next) => {
-    res.status(201).json {
-        message: 'post an answer'
-    }
-})
-
-module.exports = router;
