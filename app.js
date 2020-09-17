3cd7b6a2156b7a633b130fa06f6ff2fcb5b684d6
Priscillah Nalubega <49272077+priscillahnalubega@users.noreply.github.com>
2020-09-17 22:03:38 +0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const questionRoutes = require('./API/routes/questions');
-const answerRoutes = require('./API/routes/answers');
-
-app.use('/questions', questionRoutes);
-app.use('/answers', answerRoutes);
-
-module.exports = app;
